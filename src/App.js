10afd97bc0c91594f7bd6aca64b8e57a681b8b97
ljj33 <ljj3347@naver.com>
2023-05-19 17:17:11 +0900

    12th commit

--- src/App.js
+++ src/App.js
@@ -179,7 +179,7 @@ const App = () => {
             {
               articles.map((elem,index)=>{
                 return(
-                <Col key={index} xs={{span: 24}} lg={{span: 6}} style={{padding:"1%"}}>
+                <Col key={index} xs={{span: 12}} lg={{span: 6}} style={{padding:"1%"}}>
                   <Badge.Ribbon text={`${elem['dday']}일 남음`} count={10} size="default" style={{}}>
                   <Card title={isLoading&&articles[index]['platform']} bordered={false} style={{border:"1px solid #eee",overflow:'hidden',width:"100%",height:"100%"}} headStyle={{fontSize:"1.5rem"}}>  
                       {isLoading
